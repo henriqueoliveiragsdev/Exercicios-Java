340f69a8289f027d6b27397f6a29cb6d9153f1bc
Henrique Oliveira <pedhenriqueogomes@gmail.com>
2024-07-09 00:30:53 -0300

    Prototipo nao funcional

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+        String agencia, nomeUsuario;
+        double saldo;
+
+        System.out.println("Por favor, digite o número da conta!");
+        numero = teclado.nextInt();
+
+        System.out.println("Por favor, digite a Agência!");
+        agencia = teclado.next();
+
+        System.out.println("Agora, entre com o seu nome!");
+        nomeUsuario = "Teste";
+        nomeUsuario = teclado.nextLine();
+
+        System.out.println("Entre com o saldo para depósito, por favor!");
+        saldo = teclado.nextDouble();
+
+        System.out.println("Olá "+ nomeUsuario + ", obrigado por criar uma conta em nosso banco, sua agência é "
+        + agencia+ ", conta" + numero + "e seu sado " + saldo + " está disponível para saque.");
+
+}
 }
